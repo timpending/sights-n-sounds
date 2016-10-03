1a270ee9a4a258b92bffd2bdae041d6830d1f8dc
Tim Pender <mrtimpender@gmail.com>
2016-10-03 16:19:23 -0600

    initial commit

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,12 @@
 // Update with your config settings.
-
+require('dotenv').config({silent: true});
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      development: {
+        client: 'pg',
+        connection: 'postgres://localhost/sights-n-sounds-db'
+      },
+      production:{
+        client: 'pg',
+        connection: process.env.DATABASE_URL + '?ssl=true'
+      }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+// var knex=require(‘../db/knex’);
+// var pg = require(‘pg’);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Golf' });
 });
 
 module.exports = router;
